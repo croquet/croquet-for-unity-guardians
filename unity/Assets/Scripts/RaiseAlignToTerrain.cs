18a0490c76a249b87720d7278a59cfba92d3a090
Aran Lunzer <aran@acm.org>
2023-06-19 17:19:40 -0700

    dig structures into tilted terrain

--- unity/Assets/Scripts/RaiseAlignToTerrain.cs
+++ unity/Assets/Scripts/RaiseAlignToTerrain.cs
@@ -9,8 +9,8 @@ public class RaiseAlignToTerrain : MonoBehaviour
 {
     [HeaderAttribute("Raising")]
     public bool raiseToTerrainHeight = true;
-    public float additionalRaise = 0.0f;
-    public float raiseSpreadStrength = 0.0f;
+    public float fixedExtraRaise = 0.0f;
+    public float randomRaiseLower = 0.0f;
     public bool objectIsStatic = false;
 
     [HeaderAttribute("Alignment")] 
@@ -20,8 +20,8 @@ public class RaiseAlignToTerrain : MonoBehaviour
     private TerrainData terrainData;
     private Vector3 tPos;
     private float computedRaise;
-    private string croquetHandle;
     private CroquetSpatialComponent sc;
+    private float radius;
     
     void Start()
     {
@@ -29,7 +29,7 @@ public class RaiseAlignToTerrain : MonoBehaviour
         terrainData = terrain.terrainData;
         tPos = terrain.gameObject.transform.position;
 
-        computedRaise = additionalRaise + Random.Range(-raiseSpreadStrength, raiseSpreadStrength);
+        computedRaise = fixedExtraRaise + Random.Range(-randomRaiseLower, randomRaiseLower);
     }
 
     void Update()
@@ -41,6 +41,8 @@ public class RaiseAlignToTerrain : MonoBehaviour
             {
                 return;
             }
+
+            radius = 1.5f; // $$$ Croquet.ReadActorFloat(gameObject, "radius");
         }
         
         if (sc.hasBeenMoved)
@@ -57,15 +59,24 @@ public class RaiseAlignToTerrain : MonoBehaviour
     
     void Align()
     {
+        if (!raiseToTerrainHeight && !alignToTerrain)
+        {
+            return;
+        }
+        
+        Vector3 slopeNormal = terrainData.GetInterpolatedNormal((transform.position.x - tPos.x) / (terrainData.size.x),
+            (transform.position.z - tPos.z) / (terrainData.size.z));
+
         if (raiseToTerrainHeight)
         {
             // Raise to terrain height with additional raise amount
+            float slopeUnitOffset = Mathf.Sqrt(slopeNormal.x * slopeNormal.x + slopeNormal.z * slopeNormal.z);
             transform.position = new Vector3(
                 transform.position.x,
                 terrainData.GetInterpolatedHeight(
                     (transform.position.x - tPos.x) / (terrainData.size.x),
                     (transform.position.z - tPos.z) / (terrainData.size.z)
-                    ) + computedRaise,
+                    ) + computedRaise - slopeUnitOffset * radius,
                 transform.position.z);
         }
 
@@ -73,8 +84,7 @@ public class RaiseAlignToTerrain : MonoBehaviour
         {
             // align rotation with interpolated terrain normal
             var slopeRotation = Quaternion.FromToRotation(transform.up,
-                terrainData.GetInterpolatedNormal((transform.position.x - tPos.x) / (terrainData.size.x),
-                    (transform.position.z - tPos.z) / (terrainData.size.z)));
+                slopeNormal);
             transform.rotation = Quaternion.Slerp(transform.rotation, slopeRotation * transform.rotation,
                 alignmentStrengthNorm);
         }
