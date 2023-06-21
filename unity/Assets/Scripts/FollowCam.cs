69afcc3969ed3656973b050917023f4aeea310e2
lucas-croquet <123010049+lucas-croquet@users.noreply.github.com>
2023-06-21 16:31:29 -0700

    new bollard and followcam update

--- unity/Assets/Scripts/FollowCam.cs
+++ unity/Assets/Scripts/FollowCam.cs
@@ -10,14 +10,13 @@ public class FollowCam : MonoBehaviour
     public Vector3 rotationOffset = new Vector3(10, 0, 0);
     public float translationalLerpSpeed = 0.2f;
     public float rotationalSlerpSpeed = 0.2f;
-    //public bool noCameraRoll = true;
 
     void LateUpdate()
     {
         if (target)
         {
-            float tTug = translationalLerpSpeed * Time.deltaTime*1000/15;
-            float rTug = rotationalSlerpSpeed * Time.deltaTime*1000/15;
+            float tTug = translationalLerpSpeed * Time.deltaTime;
+            float rTug = rotationalSlerpSpeed * Time.deltaTime;
 
             Quaternion desiredRotation = Quaternion.Euler(rotationOffset.x, target.eulerAngles.y, 0);
             Vector3 desiredPosition = target.position + desiredRotation * followOffset;
