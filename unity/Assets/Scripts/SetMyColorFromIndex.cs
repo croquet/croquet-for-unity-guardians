68d08e98c3fb490c815c42c7d381d8eaa45f3dc2
Lucas <lucas.rumney@croquet.io>
2023-06-23 15:39:05 -0700

    use material instancing with property blocks

--- unity/Assets/Scripts/SetMyColorFromIndex.cs
+++ unity/Assets/Scripts/SetMyColorFromIndex.cs
@@ -6,13 +6,17 @@ public class SetMyColorFromIndex : MonoBehaviour, ICroquetDriven
 {
     public int numColors = 24;
     public Texture2D colorPallette;
-    public Material materialToSetColor;
+    public Renderer rendererToSetColor;
     
     public void CroquetInitializationComplete()
     {
         float colorIndex = Croquet.ReadActorFloat(gameObject, "colorIndex");
 
+        MaterialPropertyBlock myMaterialPropertyBlock = new MaterialPropertyBlock();
+        myMaterialPropertyBlock.SetColor("_Color", colorPallette.GetPixelBilinear((colorIndex / (float)numColors) + (1.0f/(float)numColors)/2.0f, 0.5f));
+        
+        rendererToSetColor.SetPropertyBlock(myMaterialPropertyBlock);
         // There is an extra factor of (1/numColors)/2.0 added to the U-Coordinate to sample the center of each square
-        materialToSetColor.color = colorPallette.GetPixelBilinear((colorIndex / (float)numColors) + (1.0f/(float)numColors)/2.0f, 0.5f);
+        
     }
 }
