5486ac131eaf66fced2a3a142b6c7581a69f8c25
Lucas <lucas.rumney@croquet.io>
2023-06-23 13:43:42 -0700

    add player colors and bullet colors by palette

--- unity/Assets/Scripts/SetMyColorFromIndex.cs
+++ unity/Assets/Scripts/SetMyColorFromIndex.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class SetMyColorFromIndex : MonoBehaviour, ICroquetDriven
 {
+    public int numColors = 24;
+    public Texture2D colorPallette;
+    public Material materialToSetColor;
+    
     public void CroquetInitializationComplete()
     {
         float colorIndex = Croquet.ReadActorFloat(gameObject, "colorIndex");
-        Debug.Log($"time to set {gameObject} color from colorIndex {colorIndex}");
+        Debug.Log($"time to set {gameObject} color from colorIndex {colorIndex}, u coordinate: {colorIndex/(float)numColors}");
+
+        materialToSetColor.color = colorPallette.GetPixelBilinear((colorIndex / (float)numColors), 0.5f);
     }
 }
