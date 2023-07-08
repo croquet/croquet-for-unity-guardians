f012776c78b3a0c1784a009562e8d613ed1f6b3f
Aran Lunzer <aran@acm.org>
2023-07-07 17:52:50 -0700

    a bit more simplification needed

--- unity/Assets/bounceNoise.cs
+++ unity/Assets/bounceNoise.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class bounceNoise : MonoBehaviour, ICroquetDriven
+public class bounceNoise : MonoBehaviour
 {
     private AudioSource bounceSound;
 
