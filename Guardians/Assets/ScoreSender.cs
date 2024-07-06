8d26e77e1f138a78be6c6ac36f171de71a4bb11e
ceedeepee <82616327+ceedeepee@users.noreply.github.com>
2024-07-05 21:57:12 -0400

    Update ScoreSender.cs

--- Guardians/Assets/ScoreSender.cs
+++ Guardians/Assets/ScoreSender.cs
@@ -3,37 +3,37 @@ using System.Runtime.InteropServices;
 
 public class ScoreSender : MonoBehaviour
 {
-    [DllImport("__Internal")]
-    private static extern void RequestServerTime(string walletAddress);
+    // [DllImport("__Internal")]
+    // private static extern void RequestServerTime(string walletAddress);
 
-    [DllImport("__Internal")]
-    private static extern void SubmitHighScore(string message);
+    // [DllImport("__Internal")]
+    // private static extern void SubmitHighScore(string message);
 
-    private string walletAddress;
-    private float gameDuration; // Duration in seconds
-    private int highScore;
+    // private string walletAddress;
+    // private float gameDuration; // Duration in seconds
+    // private int highScore;
 
-    public void Initialize(string wallet)
-    {
-        walletAddress = wallet;
-    }
+    // public void Initialize(string wallet)
+    // {
+    //     walletAddress = wallet;
+    // }
 
-    public void SendHighScore(int score)
-    {
-        highScore = score;
-        RequestServerTime(walletAddress);
-    }
+    // public void SendHighScore(int score)
+    // {
+    //     highScore = score;
+    //     RequestServerTime(walletAddress);
+    // }
 
-    public void ReceiveServerTime(string serverTime)
-    {
-        string message = EncodeMessage(highScore, serverTime, walletAddress, gameDuration);
-        SubmitHighScore(message);
-    }
+    // public void ReceiveServerTime(string serverTime)
+    // {
+    //     string message = EncodeMessage(highScore, serverTime, walletAddress, gameDuration);
+    //     SubmitHighScore(message);
+    // }
 
-    private string EncodeMessage(int highScore, string serverTime, string walletAddress, float gameDuration)
-    {
-        string rawMessage = $"{highScore}|{serverTime}|{walletAddress}|{gameDuration}";
-        byte[] rawMessageBytes = System.Text.Encoding.UTF8.GetBytes(rawMessage);
-        return System.Convert.ToBase64String(rawMessageBytes);
-    }
+    // private string EncodeMessage(int highScore, string serverTime, string walletAddress, float gameDuration)
+    // {
+    //     string rawMessage = $"{highScore}|{serverTime}|{walletAddress}|{gameDuration}";
+    //     byte[] rawMessageBytes = System.Text.Encoding.UTF8.GetBytes(rawMessage);
+    //     return System.Convert.ToBase64String(rawMessageBytes);
+    // }
 }
